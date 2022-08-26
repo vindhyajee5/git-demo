32e6528c131cb9ca240fce0f408461baaeb10105
Vindhyachal Prasad <vindhpr1@in.ibm.com>
2022-08-26 11:53:26 +0530

    First commit

---
+++ src/main/java/com/vindhya/Test.java
@@ -0,0 +1,14 @@
+package com.vindhya;
+
+public class Test {
+
+	 private static void printdata(String data) {
+		System.out.println("data"+data);
+
+	}
+	public static void main(String[] args) {
+		printdata("Hello");
+
+	}
+
+}
