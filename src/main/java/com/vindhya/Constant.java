72ac407f6f51d969a59a7e1eaa6d48f1ea5e25cd
Vindhyachal Prasad <vindhpr1@in.ibm.com>
2022-08-26 12:13:15 +0530

    Second Commit added constant file

---
+++ src/main/java/com/vindhya/Constant.java
@@ -0,0 +1,6 @@
+package com.vindhya;
+
+public class Constant {
+
+	public static String DATA_TYPE="HELLO";
+}
--- src/main/java/com/vindhya/Test.java
+++ src/main/java/com/vindhya/Test.java
@@ -3,6 +3,7 @@ package com.vindhya;
 public class Test {
 
 	 private static void printdata(String data) {
+	   if(Constant.DATA_TYPE.equals(data))
 		System.out.println("data"+data);
 
 	}
